cf4223ea22699fe891daef03a8dd99d1cc4bd3c2
Nathan Hui (NAHU) <nahu@odoo.com>
2023-08-02 12:25:20 +0800

    day3

---
+++ training/models/res_partner.py
@@ -0,0 +1,7 @@
+from odoo import api, fields, models
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    date_of_birth = fields.Date("Date of Birth")
+    phone = fields.Char("Phone Number")
